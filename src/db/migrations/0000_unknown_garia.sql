c51acb288f549227c16c11d4fbf1375d42dd4fc7
Divine Ekong <dekong487@gmail.com>
2025-09-07 15:26:45 +0100

    initial commit

---
+++ src/db/migrations/0000_unknown_garia.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "favourites" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"recipe_id" integer NOT NULL,
+	"title" text NOT NULL,
+	"image" text,
+	"cook_time" text,
+	"servings" text
+);
